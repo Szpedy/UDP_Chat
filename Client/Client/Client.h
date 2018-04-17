d25278ff2ea3a7874b5f1c4d10adaa632b3a4467
Patryk Romaniak <romaniak7@gmail.com>
2018-04-17 22:30:20 +0200

    Commit

---
+++ Client/Client/Client.h
@@ -0,0 +1,41 @@
+﻿#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <thread>
+#include <limits>
+// Link with ws2_32.lib
+#pragma warning(disable:4996) 
+#pragma comment(lib, "Ws2_32.lib")
+#pragma once
+
+struct packetStruct {
+	std::string Czas; // czas systemowy
+	std::string NSekwencyjny; // licznik, który gdy osi¹gnie 0 znaczy, ¿e operacja zosta³a zakoñczona
+	std::string Identyfikator; // ID klienta
+	std::string Operacja; // polaczenie | start | rozmowa | wiadomosc | zakonczenie
+	std::string Status = ""; // OK | NO
+	std::string Tekst; // wiadomosc wysylana
+					   //wszystkie górne wysy³ane oraz Operacja lub Status lub Tekst
+};
+
+std::string getTime();
+void ReadMessage(packetStruct& PS, std::string packet);
+void WriteMessage(packetStruct& PS, std::string& packet);
+void ZeroPS(packetStruct& PS);
+void printMSG(const packetStruct &PS);
+int SendPacket(SOCKET s, addrinfo *SendAddr, packetStruct &PS, std::string packet);
+int RecievePacket(SOCKET s, sockaddr_in &RecvAddr, packetStruct &PS, std::string packet, int RecvAddrSize);
+int SendAck(SOCKET s, addrinfo *SendAddr, packetStruct PS, std::string packet);
+void Recieve(SOCKET s, sockaddr_in RecvAddr, addrinfo *SendAddr, packetStruct PS, std::string packet);
+int delay(int &value);
---
+++ Server/Server/Server.h
@@ -0,0 +1,54 @@
+﻿#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <bitset>
+#include <random>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <ctime>
+
+// Link with ws2_32.lib
+#pragma warning(disable:4996) 
+#pragma comment(lib, "Ws2_32.lib")
+#pragma once
+
+struct packetStruct {
+	std::string Czas; // czas systemowy
+	std::string NSekwencyjny; // licznik, który gdy osi¹gnie 0 znaczy, ¿e operacja zosta³a zakoñczona
+	std::string Identyfikator; // ID klienta
+	std::string Operacja; // polaczenie | start | rozmowa | wiadomosc | zakonczenie
+	std::string Status; // OK | NO
+	std::string Tekst; // wiadomosc wysylana
+					   //wszystkie górne wysy³ane oraz Operacja lub Status lub Tekst
+};
+
+struct clientStruct
+{
+	sockaddr_in addr;
+	std::string ID;
+	packetStruct PS;
+};
+
+std::string getTime();
+std::string getID();
+std::string Clientinfo(sockaddr_in Client);
+void ReadMessage(packetStruct& PS, std::string packet);
+void ZeroPS(packetStruct& PS);
+void WriteMessage(packetStruct& PS, std::string& packet);
+void printMSG(const packetStruct &PS);
+int RecievePacket(SOCKET s, sockaddr_in &RecvAddr, packetStruct &PS, std::string& packet, int RecvAddrSize);
+int SendPacket(SOCKET s, sockaddr_in SendAddr, packetStruct &PS, std::string& packet);
+int SendAck(SOCKET s, sockaddr_in SendAddr, packetStruct PS, std::string packet);
+int recipient(int who);
+void handlePacket(SOCKET s, sockaddr_in Client, std::string& packet, packetStruct &PS, int who);
+int whoSentDatagram(std::vector<clientStruct>& Clients, sockaddr_in Client, packetStruct PS, std::string packet);
